adfe4583a78c8f4143abda07042ea092cd38050b
vera-com <withneyugomma@gmail.com>
2026-03-27 14:29:42 +0100

    Add course model and update studysession relationship

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import StudySession
+from .models import StudySession, Course
 
+
+admin.site.register(Course)
 admin.site.register(StudySession)
--- tracker/models.py
+++ tracker/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class StudySession(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     duration = models.IntegerField()
