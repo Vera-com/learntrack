255a2390437449e6fb424010437eba53afd76322
vera-com <withneyugomma@gmail.com>
2026-04-07 16:31:30 +0200

    Fix signals setup for superuser creation.

---
+++ tracker/signals.py
@@ -0,0 +1,24 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+
+@receiver(post_migrate)
+def create_superuser(sender, **kwargs):
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if not username or not email or not password:
+        return
+
+    User = get_user_model()
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
